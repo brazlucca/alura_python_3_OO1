f0bd8afe33cc6f65fbf46dd4831057eee47982f8
brazlucca <brazlucca@gmail.com>
2022-02-02 00:50:50 -0300

    commit

--- conta.py
+++ conta.py
@@ -1,3 +1,4 @@
+
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
@@ -19,3 +20,18 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    #def get_numero(self):
+    #    return self.__numero
+
+    def set_limite(self, limite):
+        self.__limite = limite
